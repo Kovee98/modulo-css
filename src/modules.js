659d584b6f3fb7748f5edbf3838b084aeab15031
Joshua Kovalchik <kovee98@gmail.com>
2020-10-04 14:06:16 -0500

    feat(module): add base typography module

--- src/modules.js
+++ src/modules.js
@@ -7,5 +7,10 @@ export default [
         name: 'spacing',
         desc: 'Classes for margins and padding.',
         isChecked: false
+    },
+    {
+        name: 'typography',
+        desc: 'Classes for text and headings',
+        isChecked: false
     }
 ];
