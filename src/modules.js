125671dee74e41ecb54aa59fff08c31737d97896
Joshua Kovalchik <kovee98@gmail.com>
2020-10-07 07:38:24 -0500

    docs(misc): fix desc wording for misc module

--- src/modules.js
+++ src/modules.js
@@ -35,7 +35,7 @@ export default [
     },
     {
         name: 'misc',
-        desc: 'Classes for random little helper classes',
+        desc: 'Classes for random little helpers',
         isChecked: false
     }
 ];
